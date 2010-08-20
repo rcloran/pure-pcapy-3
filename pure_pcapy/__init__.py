cff38eb94ffa648015388e7ed8e8d35c6e9357df
Stanis?aw Pitucha <viraptor@gmail.com>
2010-08-20 20:52:03 +0100

    adding initial versions of the lib

---
+++ pure_pcapy/__init__.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+""" pcapy clone in pure python """
+
+import struct
+
+class PcapError(Exception): pass
+
+def fixup_identical_short(x): return x
+def fixup_identical_long(x): return x
+def fixup_swapped_short(x):
+	return ((x&0xff) << 8) | ((x&0xff00) >> 8)
+def fixup_swapped_long(x):
+	bottom = fixup_swapped_short(x & 0xffff)
+	top = fixup_swapped_short((x >> 16) & 0xffff)
+	return ((bottom << 16) & 0xffff0000) | top
+
+fixup_sets = {
+		0xa1b2c3d4: (fixup_identical_short, fixup_identical_long),
+		0xd4c3b2a1: (fixup_swapped_short, fixup_swapped_long),
+		}
+
+def open_offline(filename):
+	if filename == "-":
+		import sys
+		source = sys.stdin
+	else:
+		try:
+			source = open(filename, "rb")
+		except Exception, e:
+			raise PcapError("file access problem", e)
+	
+	return Reader(source)
+
+def open_live(device, snaplen, promisc, to_ms):
+	raise NotImplementedError("This function is only available in pcapy")
+
+def lookupdev():
+	raise NotImplementedError("This function is only available in pcapy")
+
+def findalldevs():
+	raise NotImplementedError("This function is only available in pcapy")
+
+def compile(linktype, snaplen, filter, optimize, netmask):
+	raise NotImplementedError("not implemented yet")
+
+class Reader(object):
+	GLOBAL_HEADER_LEN = 24
+	PACKET_HEADER_LEN = 16
+
+	def __init__(self, source):
+		self.source = source
+		header = self.source.read(self.GLOBAL_HEADER_LEN)
+		if len(header) < self.GLOBAL_HEADER_LEN:
+			raise PcapError("truncated dump file; tried to read %i file header bytes, only got %i" % (self.GLOBAL_HEADER_LEN, len(header)))
+		
+		hdr_values = struct.unpack("IHHIIII", header)
+		if hdr_values[0] in fixup_sets:
+			self.fixup_short, self.fixup_long = fixup_sets[hdr_values[0]]
+		else:
+			raise PcapError("bad dump file format")
+
+		self.version_major, self.version_minor = [self.fixup_short(x) for x in hdr_values[1:3]]
+		self.thiszone, self.sigfigs, self.snaplen, self.network = [self.fixup_long(x) for x in hdr_values[3:]]
+
+		self.last_good_position = self.GLOBAL_HEADER_LEN
+
+	def dispatch(self, maxcant, callback):
+		pass
+
+	def loop(self, maxcant, callback):
+		pass
+
+	def next(self):
+		header = self.source.read(self.PACKET_HEADER_LEN)
+		hdr_values = struct.unpack("IIII", header)
+		ts_sec, ts_usec, incl_len, orig_len = [self.fixup_long(x) for x in hdr_values]
+		
+		pkthdr = Pkthdr(ts_sec, ts_usec, incl_len, orig_len)
+		data = self.source.read(incl_len)
+		return (pkthdr, data)
+
+	def getnet(self):
+		raise NotImplementedError("This function is only available in pcapy")
+
+	def getmask(self):
+		raise NotImplementedError("This function is only available in pcapy")
+
+	def datalink(self):
+		return self.network
+
+	def getnonblock(self):
+		return 0
+
+	def setnonblock(self, state):
+		""" this has no effect on savefiles, so is not implemented in pure-pcapy """
+		pass
+
+class Dumper(object):
+	def __init__(self):
+		pass
+
+	def dump(self, header, data):
+		pass
+
+class Pkthdr(object):
+	def __init__(self, ts_sec, ts_usec, incl_len, orig_len):
+		self.ts = (ts_sec, ts_usec)
+		self.incl_len = incl_len
+		self.orig_len = orig_len
+
+	def getts(self):
+		return self.ts
+
+	def getcaplen(self):
+		return self.incl_len
+
+	def getlen(self):
+		return self.orig_len
+
+class Bpf(object):
+	def __init__(self):
+		raise NotImplementedError("not implemented yet")
+
+	def filter(self, packet):
+		raise NotImplementedError("not implemented yet")
+
---
+++ tests.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+import unittest, tempfile, struct
+import pure_pcapy
+
+class OpenerTest(unittest.TestCase):
+	def test_open_nonexistant(self):
+		self.assertRaises(pure_pcapy.PcapError, pure_pcapy.open_offline, 'hufioewhauiefhshcuidhuighureiahufiesa')
+	def test_open_directory(self):
+		self.assertRaises(pure_pcapy.PcapError, pure_pcapy.open_offline, './')
+	
+	def test_open_good_file(self):
+		self.fail('implement this')
+
+class FixupTest(unittest.TestCase):
+	def test_fixup_short(self):
+		self.assertEquals(0x3412, pure_pcapy.fixup_swapped_short(0x1234))
+	def test_fixup_long(self):
+		self.assertEquals(0x78563412, pure_pcapy.fixup_swapped_long(0x12345678))
+
+def create_pcap_file(parts):
+	input = tempfile.TemporaryFile()
+	for part in parts:
+		input.write(part)
+	input.seek(0)
+	return input
+
+class ReaderHeaderTest(unittest.TestCase):
+	def test_open_short(self):
+		input = tempfile.TemporaryFile()
+		try:
+			pure_pcapy.Reader(input)
+			self.fail("exception not thrown")
+		except pure_pcapy.PcapError, e:
+			self.assertEqual("truncated dump file; tried to read 24 file header bytes, only got 0", e.args[0])
+	
+	def test_open_header_bad(self):
+		input = create_pcap_file([
+			struct.pack("IHHIIII", 0x11111111, 2, 4, 0, 0, 65535, 1)
+			])
+
+		try:
+			pure_pcapy.Reader(input)
+			self.fail("exception not thrown")
+		except pure_pcapy.PcapError, e:
+			self.assertEqual("bad dump file format", e.args[0])
+	
+	def test_open_header_bigendian(self):
+		input = create_pcap_file([
+			struct.pack(">IHHIIII", 0xa1b2c3d4, 2, 4, 0, 0, 65535, 1)
+			])
+
+		reader = pure_pcapy.Reader(input)
+		self.assertEqual(2, reader.version_major)
+		self.assertEqual(4, reader.version_minor)
+	
+	def test_open_header_littleendian(self):
+		input = create_pcap_file([
+			struct.pack("<IHHIIII", 0xa1b2c3d4, 2, 4, 0, 0, 65535, 1)
+			])
+
+		reader = pure_pcapy.Reader(input)
+		self.assertEqual(2, reader.version_major)
+		self.assertEqual(4, reader.version_minor)
+	
+	def test_datalink(self):
+		input = create_pcap_file([
+			struct.pack("IHHIIII", 0xa1b2c3d4, 2, 4, 0, 0, 65535, 1234)
+			])
+
+		reader = pure_pcapy.Reader(input)
+		self.assertEqual(1234, reader.datalink())
+
+	def test_getnonblock(self):
+		input = create_pcap_file([
+			struct.pack("IHHIIII", 0xa1b2c3d4, 2, 4, 0, 0, 65535, 1234)
+			])
+
+		reader = pure_pcapy.Reader(input)
+		self.assertEqual(0, reader.getnonblock())
+
+class ReaderPacketTest(unittest.TestCase):
+	def test_read_packet(self):
+		input = create_pcap_file([
+			struct.pack("IHHIIII", 0xa1b2c3d4, 2, 4, 0, 0, 65535, 1),
+			struct.pack("IIII", 10, 20, 30, 40),
+			"x" * 30
+			])
+
+		reader = pure_pcapy.Reader(input)
+		res = reader.next()
+		self.assertEqual(tuple, type(res))
+		self.assertEqual(2, len(res))
+		self.assertTrue(isinstance(res[0], pure_pcapy.Pkthdr))
+		self.assertEqual(str, type(res[1]))
+
+class PkthdrTest(unittest.TestCase):
+	def test_hdr_fields(self):
+		input = create_pcap_file([
+			struct.pack("IHHIIII", 0xa1b2c3d4, 2, 4, 0, 0, 65535, 1),
+			struct.pack("IIII", 10, 20, 30, 40),
+			"x" * 30
+			])
+		
+		reader = pure_pcapy.Reader(input)
+		hdr, data = reader.next()
+		self.assertEqual((10, 20), hdr.getts())
+		self.assertEqual(30, hdr.getcaplen())
+		self.assertEqual(40, hdr.getlen())
+
+if __name__ == "__main__":
+	unittest.main()
